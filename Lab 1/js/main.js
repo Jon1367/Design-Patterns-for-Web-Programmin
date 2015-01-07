51bbc8bb7e3417b74d6a95dd52d494f7ff8183be
Jonathan Ortiz <jonathano1367@yahoo.com>
2015-01-06 17:25:20 -0800

    Added Function and DeBug
    Add Delete Feature

--- Lab 1/js/main.js
+++ Lab 1/js/main.js
@@ -4,6 +4,8 @@
 	Created by Jonathan E. Ortiz
 */
 
+var movieList = ["Star Wars",];
+
 (function(){
 
 
@@ -21,23 +23,38 @@
 
  	prompt(" Hello " + userName + " it's nice to meet you");
 
- 	var movieList = ["Star Wars",];
+ 	
 
 
  	prompt("Welcome " + userName + " your Movie List");
 
- 	while(input != 4){
+ 	while(input != 3){
 
 
- 		for( var i = 0; i < movieList.length;i++){
  		input = prompt(userName + "'s Movie List" + "\n" +
  		"----------------------------" + "\n" +
- 		 movieList[i] + "\n" +
+ 		 movieList + "\n" +
 		"----------------------------" + "\n" +
 		"1)Add" + "\n" +
-		"2)Delete" + "\n");
- 		}
+		"2)Delete" + "\n"+
+		"3)Quit");
+ 		
 
+ 		if( input == 1){
+
+ 			movie = prompt("Enter a movie:");
+
+ 			Add(movie);
+
+ 		}else if(input == 2){
+
+ 			dMovie = prompt(" Whch Movie do you want to remove:" + "\n" +
+ 				movieList+" " + "\n"+
+ 				" Enter the number of the movie in the list. Starts at 1 After Star Wars");
+
+ 			Delete(dMovie);
+
+ 		}
 
  	}
 
@@ -50,4 +67,22 @@ function FullName(first, last){
 	var fullname = first + " " + last 
 
 	return fullname;
+}
+function Add(m){
+
+	movieList.push(m);
+
+}
+function Delete(m){
+
+	if(parseInt(m) == 1){
+
+	var counter =  parseInt(m);
+
+	}else{
+		var counter =  parseInt(m) - 1;
+	}
+
+	movieList.splice(m,counter);
+
 }
